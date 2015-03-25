08d1b2a908fa2b881c9b08aa051e026a7f6c6631
cschen <cschen@marvell.com>
2015-03-25 14:41:41 +0800

    Add present/

--- clean.sh
+++ clean.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
-pushd lbdex
-pushd input
-bash clean.sh
-popd
-pushd verilog
-bash clean.sh
-popd
-popd
+if test -d lbdex; then
+  pushd lbdex
+  if test -d input; then
+    pushd input
+    bash clean.sh
+    popd
+  fi
+  if test -d verilog; then
+    pushd verilog
+    bash clean.sh
+    popd
+  fi
+  popd
+fi
 
-pushd exlbt
-pushd input
-bash clean.sh
-popd
-pushd libsoftfloat/compiler-rt
-bash clean.sh
-popd
-pushd libc/avr-libc-1.8.1
-bash clean.sh
-popd
-popd
+if test -d exlbt; then
+  pushd exlbt
+  if test -d input; then
+    pushd input
+    bash clean.sh
+    popd
+  fi
+  if test -d libsoftfloat/compiler-rt; then
+    pushd libsoftfloat/compiler-rt
+    bash clean.sh
+    popd
+  fi
+  if test -d libc/avr-libc-1.8.1; then
+    pushd libc/avr-libc-1.8.1
+    bash clean.sh
+    popd
+  fi
+  popd
+fi
 rm -rf build exlbt.tar.gz
 rm -f `find . -name \*~`
 rm -f `find . -name .DS_Store`
