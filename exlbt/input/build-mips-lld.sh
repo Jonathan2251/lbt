9e451ce261323adcb59e73b5ffc7ccdf5beaecd9
cschen <cschen@marvell.com>
2015-09-10 09:01:44 +0800

    build-mips-lld.sh

--- exlbt/input/build-mips-lld.sh
+++ exlbt/input/build-mips-lld.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+
+endian=
+#endian=el
+
 OS=`uname -s`
 echo "OS =" ${OS}
 
 if [ "$OS" == "Linux" ]; then
-  TOOLDIR=/usr/local/llvm/test/cmake_debug_build/bin
+  TOOLDIR=~/llvm/release/cmake_debug_build/bin
 else
   TOOLDIR=~/test/lld/cmake_debug_build/Debug/bin
 fi
@@ -14,15 +18,15 @@ clang -target mips-unknown-linux-gnu -c printf-stdarg-def.c -emit-llvm \
 -o printf-stdarg-def.bc
 clang -target mips-unknown-linux-gnu -c printf-stdarg.c -emit-llvm \
 -o printf-stdarg.bc
-clang -c ch_c++_1.cpp -emit-llvm -DTEST_RUN -o ch_c++_1.bc
-clang -O1 -target mips-unknown-linux-gnu -c ch12_4.cpp -emit-llvm -o ch12_4.bc
-${TOOLDIR}/llc -march=mips -relocation-model=static -filetype=obj \
+clang -target mips-unknown-linux-gnu -c ch_hello.c -emit-llvm -DTEST_RUN -o ch_hello.bc
+${TOOLDIR}/llc -march=mips${endian} -relocation-model=static -filetype=obj \
 printf-stdarg-def.bc -o printf-stdarg-def.o
-${TOOLDIR}/llc -march=mips -relocation-model=static -filetype=obj \
+${TOOLDIR}/llc -march=mips${endian} -relocation-model=static -filetype=obj \
 printf-stdarg.bc -o printf-stdarg.o
-${TOOLDIR}/llc -march=mips -relocation-model=static -filetype=obj \
-ch_c++_1.bc -o ch_c++_1.o
-${TOOLDIR}/llc -march=mips -relocation-model=static -filetype=obj \
+${TOOLDIR}/llc -march=mips${endian} -relocation-model=static -filetype=obj \
+ch_hello.bc -o ch_hello.o
+${TOOLDIR}/llc -march=mips${endian} -relocation-model=static -filetype=obj \
 lib_cpu0.ll -o lib_cpu0.o
-${TOOLDIR}/lld -flavor gnu -target mipsel-unknown-linux-gnu \
-printf-stdarg-def.o printf-stdarg.o ch_c++_1.o lib_cpu0.o \
+~/llvm/test/cmake_release_build/bin/lld -flavor gnu -target \
+mips${endian}-unknown-linux-gnu printf-stdarg-def.o printf-stdarg.o ch_hello.o \
+lib_cpu0.o \
