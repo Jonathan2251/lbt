6cac7fb698d28788b3894d993a93852dcec2a573
Jonathan <gamma_chen@yahoo.com.tw>
2015-11-02 08:14:02 +0800

    build-slinker.sh

--- exlbt/input/build-slinker.sh
+++ exlbt/input/build-slinker.sh
@@ -15,8 +15,8 @@ clang -target mips-unknown-linux-gnu -c printf-stdarg-def.c -emit-llvm \
 -o printf-stdarg-def.bc
 clang -target mips-unknown-linux-gnu -c printf-stdarg.c -emit-llvm \
 -o printf-stdarg.bc
-clang -target mips-unknown-linux-gnu -c ${LBDEXDIR}/input/ch4_1_2.cpp \
--emit-llvm -o ch4_1_2.bc
+clang -target mips-unknown-linux-gnu -c ${LBDEXDIR}/input/ch4_1_addsuboverflow.cpp \
+-emit-llvm -o ch4_1_addsuboverflow.bc
 clang -target mips-unknown-linux-gnu -c ${LBDEXDIR}/input/ch8_1_5.cpp \
 -emit-llvm -o ch8_1_5.bc
 clang -O3 -target mips-unknown-linux-gnu -c ${LBDEXDIR}/input/ch8_2_phinode.cpp \
@@ -30,34 +30,35 @@ clang -target mips-unknown-linux-gnu -c ${LBDEXDIR}/input/ch9_3_detect_exception
 clang -I${LBDEXDIR}/input/ -target mips-unknown-linux-gnu -c ch_slinker.cpp \
 -emit-llvm -o ch_slinker.bc
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj start.bc -o start.cpu0.o
+-filetype=obj start.bc -o start.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj debug.bc -o debug.cpu0.o
+-filetype=obj debug.bc -o debug.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj printf-stdarg-def.bc -o printf-stdarg-def.cpu0.o
+-filetype=obj printf-stdarg-def.bc -o printf-stdarg-def.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj printf-stdarg.bc -o printf-stdarg.cpu0.o
+-filetype=obj printf-stdarg.bc -o printf-stdarg.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj -cpu0-enable-overflow=true ch4_1_2.bc -o ch4_1_2.cpu0.o
+-filetype=obj -cpu0-enable-overflow=true ch4_1_addsuboverflow.bc -o \
+ch4_1_addsuboverflow.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj ch8_1_5.bc -o ch8_1_5.cpu0.o
+-filetype=obj ch8_1_5.bc -o ch8_1_5.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj ch8_2_phinode.bc -o ch8_2_phinode.cpu0.o
+-filetype=obj ch8_2_phinode.bc -o ch8_2_phinode.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj ch8_blockaddr.bc -o ch8_blockaddr.cpu0.o
+-filetype=obj ch8_blockaddr.bc -o ch8_blockaddr.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=pic \
 -filetype=obj -force-cpu0-long-branch ch8_2_longbranch.bc -o \
-ch8_2_longbranch.cpu0.o
+ch8_2_longbranch.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj ch9_3_detect_exception.bc -o ch9_3_detect_exception.cpu0.o
+-filetype=obj ch9_3_detect_exception.bc -o ch9_3_detect_exception.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj ch_slinker.bc -o ch_slinker.cpu0.o
+-filetype=obj ch_slinker.bc -o ch_slinker.o
 ${TOOLDIR}/llc -march=cpu0${endian} -mcpu=${CPU} -relocation-model=static \
--filetype=obj lib_cpu0.ll -o lib_cpu0.o
-${TOOLDIR}/lld -flavor gnu -target cpu0${endian}-unknown-linux-gnu start.cpu0.o \
-debug.cpu0.o printf-stdarg-def.cpu0.o printf-stdarg.cpu0.o ch4_1_2.cpu0.o \
-ch8_1_5.cpu0.o ch8_2_phinode.cpu0.o ch8_blockaddr.cpu0.o ch8_2_longbranch.cpu0.o \
-ch9_3_detect_exception.cpu0.o ch_slinker.cpu0.o lib_cpu0.o -o a.out
+-filetype=obj lib_cpu0.ll -o lib_o
+${TOOLDIR}/lld -flavor gnu -target cpu0${endian}-unknown-linux-gnu start.o \
+debug.o printf-stdarg-def.o printf-stdarg.o ch4_1_addsuboverflow.o \
+ch8_1_5.o ch8_2_phinode.o ch8_blockaddr.o ch8_2_longbranch.o \
+ch9_3_detect_exception.o ch_slinker.o lib_o -o a.out
 
 epilogue;
 
