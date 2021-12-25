a355cbde5c2b14a045ce5e96605e53f0c8861b04
ChungShu <gamma_chen@yahoo.com.tw>
2021-12-25 15:44:33 +0800

    check_result() for ch_float_necessary.cpp

--- exlbt/input/ch_float_necessary.cpp
+++ exlbt/input/ch_float_necessary.cpp
@@ -39,55 +39,64 @@ T1 test_div(T2 a, T3 b) {
   return c;
 }
 
+template <class T>
+bool check_result(const char* fn, T res, T expected) {
+  printf("%s\n", fn);
+  if (res != expected) {
+    printf("\terror: result %d, expected %d\n", res, expected);
+  }
+  return (res == expected);
+}
+
 int main() {
   int a;
 
   a = test_longlong_shift1();
-  printf("test_longlong_shift1() = %d\n", a);
+  check_result("test_longlong_shift1()", a, 289);
 
   a = test_longlong_shift2();
-  printf("test_longlong_shift2() = %d\n", a);
+  check_result("test_longlong_shift2()", a, 22);
 
 // call __ashldi3
   a = (int)test_shift_left<long long>(0x12, 4); // 0x120 = 288
-  printf("(int)test_shift_left<long long>(0x12, 4) = %d\n", a);
+  check_result("(int)test_shift_left<long long>(0x12, 4)", a, 288);
   
 // call __ashrdi3
   a = (int)test_shift_right<long long>(0x001666660000000a, 48); // 0x16 = 22
-  printf("(int)test_shift_right<long long>(0x001666660000000a, 48) = %d\n", a);
+  check_result("(int)test_shift_right<long long>(0x001666660000000a, 48)", a, 22);
   
 // call __lshrdi3
   a = (int)test_shift_right<unsigned long long>(0x001666660000000a, 48); // 0x16 = 22
-  printf("(int)test_shift_right<unsigned long long>(0x001666660000000a, 48) = %d\n", a);
+  check_result("(int)test_shift_right<unsigned long long>(0x001666660000000a, 48)", a, 22);
   
 // call __addsf3, __fixsfsi
   a = (int)test_add<float, float, float>(-2.2, 3.3); // (int)1.1 = 1
-  printf("(int)test_add<float, float, float>(-2.2, 3.3) = %d\n", a);
+  check_result("(int)test_add<float, float, float>(-2.2, 3.3)", a, 1);
   
 // call __mulsf3, __fixsfsi
   a = (int)test_mul<float, float, float>(-2.2, 3.3); // (int)-7.26 = -7
-  printf("(int)test_mul<float, float, float>(-2.2, 3.3) = %d\n", a);
+  check_result("(int)test_mul<float, float, float>(-2.2, 3.3)", a, -7);
   
 // call __divsf3, __fixsfsi
   a = (int)test_div<float, float, float>(-1.8, 0.5); // (int)-3.6 = -3
-  printf("(int)test_div<float, float, float>(-1.8, 0.5) = %d\n", a);
+  check_result("(int)test_div<float, float, float>(-1.8, 0.5)", a, -3);
   
 // call __extendsfdf2, __adddf3, __fixdfsi
   a = (int)test_add<double, double, float>(-2.2, 3.3); // (int)1.1 = 1
-  printf("(int)test_add<double, double, float>(-2.2, 3.3) = %d\n", a);
+  check_result("(int)test_add<double, double, float>(-2.2, 3.3)", a, 1);
   
 // call __extendsfdf2, __muldf3, __fixdfsi
   a = (int)test_mul<double, float, double>(-2.2, 3.3); // (int)-7.26 = -7
-  printf("(int)test_mul<double, float, double>(-2.2, 3.3) = %d\n", a);
+  check_result("(int)test_mul<double, float, double>(-2.2, 3.3)", a, -7);
   
 // call __extendsfdf2, __muldf3, __truncdfsf2, __fixdfsi
 // ! __truncdfsf2 in truncdfsf2.c is not work for Cpu0
   a = (int)test_mul<float, float, double>(-2.2, 3.3); // (int)-7.26 = -7
-  printf("(int)test_mul<float, float, double>(-2.2, 3.3) = %d\n", a);
+  check_result("(int)test_mul<float, float, double>(-2.2, 3.3)", a, -7);
   
 // call __divdf3, __fixdfsi
   a = (int)test_div<double, double, double>(-1.8, 0.5); // (int)-3.6 = -3
-  printf("(int)test_div<double, double, double>(-1.8, 0.5) = %d\n", a);
+  check_result("(int)test_div<double, double, double>(-1.8, 0.5)", a, -3);
   
   return 0;
 }
