39de8d446aab29bb6cc080512d7b481a8e5aa351
Jonathan2251 <gamma_chen@yahoo.com.tw>
2022-01-31 13:39:44 +0800

    fix bug of newlib-cpu0.sh

--- exlbt/newlib-cpu0.sh
+++ exlbt/newlib-cpu0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NEWLIB_PARENT_DIR=$HOME/git/1
+NEWLIB_PARENT_DIR=$HOME/git/2
 CURR_DIR=`pwd`
 
 install_newlib() {
@@ -8,8 +8,7 @@ install_newlib() {
   git clone git://sourceware.org/git/newlib-cygwin.git
   cd newlib-cygwin
   git checkout dcb25665be227fb5a05497b7178a3d5df88050ec
-  cp $CURR_DIR/newlib.patch newlib-cygwin/.
-  cd newlib-cygwin
+  cp $CURR_DIR/newlib.patch .
   git apply newlib.patch
   cp -rf $CURR_DIR/newlib-cygwin/newlib/libc/machine/cpu0 newlib/libc/machine/. 
   popd
@@ -21,6 +20,7 @@ build_newlib() {
   cd build
   CC=/Users/cschen/llvm/test/build/bin/clang CFLAGS="-static -fintegrated-as -Wno-error=implicit-function-declaration" AS="/Users/cschen/llvm/test/build/bin/clang -static -fintegrated-as -c" AR="/Users/cschen/llvm/test/build/bin/llvm-ar" RANLIB="/Users/cschen/llvm/test/build/bin/llvm-ranlib" READELF="/Users/cschen/llvm/test/build/bin/llvm-readelf" ../newlib/configure --host=cpu0
   make
+  popd
 }
 
 install_newlib;
