60caf3127aa744450f5ae94a89f493aa07a6adb1
Jonathan2251 <gamma_chen@yahoo.com.tw>
2022-02-03 18:24:45 +0800

    Replace lib_cpu0.ll with lib_cpu0.c

---
+++ exlbt/input/lib_cpu0.c
@@ -0,0 +1,12 @@
+void _start() {}
+void __stack_chk_fail() {}
+void __stack_chk_guard() {}
+void _ZdlPv() {}
+void __dso_handle() {}
+void _ZNSt8ios_base4InitC1Ev() {}
+void __cxa_atexit() {}
+void _ZTVN10__cxxabiv120__si_class_type_infoE() {}
+void _ZTVN10__cxxabiv117__class_type_infoE() {}
+void _Znwm() {}
+void __cxa_pure_virtual() {}
+void ZNSt8ios_base4InitD1Ev() {}
