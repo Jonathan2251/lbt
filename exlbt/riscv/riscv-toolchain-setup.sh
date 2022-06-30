c0a130053c5ce977ae408aa6e923b95f812df072
Jonathan2251 <gamma_chen@yahoo.com.tw>
2022-06-30 19:20:57 +0800

    wrap for long line

--- exlbt/riscv/riscv-toolchain-setup.sh
+++ exlbt/riscv/riscv-toolchain-setup.sh
@@ -51,7 +51,8 @@ build_gnu_toolchain() {
 # commit 409b951ba6621f2f115aebddfb15ce2dd78ec24f of master branch is work
   mkdir build_newlib
   cd build_newlib
-  ../configure --prefix=$GNU_NEWLIB_INSTALL_DIR --with-multilib-generator="rv32i-ilp32--;rv32imafd-ilp32--;rv64ima-lp64--"
+  ../configure --prefix=$GNU_NEWLIB_INSTALL_DIR \
+  --with-multilib-generator="rv32i-ilp32--;rv32imafd-ilp32--;rv64ima-lp64--"
   make
 
   cd ..
@@ -62,18 +63,32 @@ build_gnu_toolchain() {
   popd
 }
 
-# DEFAULT_SYSROOT: https://stackoverflow.com/questions/66357013/compile-clang-with-alternative-sysroot
-# LLVM_DEFAULT_TARGET_TRIPLE:  https://clang.llvm.org/docs/CrossCompilation.html#general-cross-compilation-options-in-clang
+# DEFAULT_SYSROOT: 
+#   https://stackoverflow.com/questions/66357013/compile-clang-with-alternative-sysroot
+# LLVM_DEFAULT_TARGET_TRIPLE:  
+#   https://clang.llvm.org/docs/CrossCompilation.html#general-cross-compilation-options-in-clang
 # Use "clang --sysroot" if did not "cmake -DDEFAULT_SYSROOT"
-# $GNU_NEWLIB_INSTALL_DIR/clang --gcc-toolchain=$GNU_NEWLIB_INSTALL_DIR --sysroot=$GNU_NEWLIB_INSTALL_DIR/sysroot/ --static test.c
+# $GNU_NEWLIB_INSTALL_DIR/clang --gcc-toolchain=$GNU_NEWLIB_INSTALL_DIR \
+#   --sysroot=$GNU_NEWLIB_INSTALL_DIR/sysroot/ --static test.c
 build_llvm_toolchain() {
   pushd $LLVM_NEWLIB_BUILD_DIR
-  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD="RISCV" -DLLVM_ENABLE_PROJECTS="clang;lld"  -DLLVM_BINUTILS_INCDIR=$GNU_SRC_DIR/riscv-gnu-toolchain/riscv-binutils/include  -DCMAKE_INSTALL_PREFIX=$GNU_NEWLIB_INSTALL_DIR -DLLVM_PARALLEL_COMPILE_JOBS=4  -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-elf -DDEFAULT_SYSROOT=$GNU_NEWLIB_INSTALL_DIR/riscv64-unknown-elf -DLLVM_INSTALL_UTILS=ON ../llvm
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD="RISCV" \
+  -DLLVM_ENABLE_PROJECTS="clang;lld"  \
+  -DLLVM_BINUTILS_INCDIR=$GNU_SRC_DIR/riscv-gnu-toolchain/riscv-binutils/include \
+  -DCMAKE_INSTALL_PREFIX=$GNU_NEWLIB_INSTALL_DIR -DLLVM_PARALLEL_COMPILE_JOBS=4 \
+  -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-elf \
+  -DDEFAULT_SYSROOT=$GNU_NEWLIB_INSTALL_DIR/riscv64-unknown-elf \
+  -DLLVM_INSTALL_UTILS=ON ../llvm
   ninja
   ninja install
   popd
   pushd $LLVM_LINUX_BUILD_DIR
-  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD="RISCV" -DLLVM_ENABLE_PROJECTS="clang;lld"  -DLLVM_BINUTILS_INCDIR=$GNU_SRC_DIR/riscv-gnu-toolchain/riscv-binutils/include  -DCMAKE_INSTALL_PREFIX=$GNU_LINUX_INSTALL_DIR -DLLVM_PARALLEL_COMPILE_JOBS=4  -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-linux-gnu -DDEFAULT_SYSROOT=$GNU_LINUX_INSTALL_DIR/sysroot -DLLVM_INSTALL_UTILS=ON ../llvm
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD="RISCV" \
+  -DLLVM_ENABLE_PROJECTS="clang;lld"  \
+  -DLLVM_BINUTILS_INCDIR=$GNU_SRC_DIR/riscv-gnu-toolchain/riscv-binutils/include \
+  -DCMAKE_INSTALL_PREFIX=$GNU_LINUX_INSTALL_DIR -DLLVM_PARALLEL_COMPILE_JOBS=4 \
+  -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-linux-gnu \
+  -DDEFAULT_SYSROOT=$GNU_LINUX_INSTALL_DIR/sysroot -DLLVM_INSTALL_UTILS=ON ../llvm
   ninja
   ninja install
   popd
